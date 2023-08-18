d8cdc82aafe6c5e6241a59da4e98b6e9f179f683
Charma Whorms <127371328+charmawhorms@users.noreply.github.com>
2023-08-18 09:52:27 -0500

    added experience image fields

--- DolphinCove/Models/Experience.cs
+++ DolphinCove/Models/Experience.cs
@@ -12,5 +12,14 @@ namespace DolphinCove.Models
 
         [Required]
         public decimal ExperiencePrice { get; set; }
+
+        [Required]
+        public string ExperienceImage1 { get; set; }
+
+        public string? ExperienceImage2 { get; set; }
+
+        public string? ExperienceImage3 { get; set; }
+
+        public string? ExperienceImage4 { get; set; }
     }
 }
