db7a27390211c2bfd08b5c617207546c658101b7
charmawhorms <charmawhorms27@gmail.com>
2023-08-17 20:08:28 -0500

    initial model

---
+++ DolphinCove/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using DolphinCove.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DolphinCove.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Addon> Addons { get; set; }
+
+        public DbSet<Cruise> Cruises { get; set; }
+
+        public DbSet<PromotionCode> PromotionCodes { get; set; }
+
+        public DbSet<Experience> Experiences { get; set; }
+
+        public DbSet<Park> Parks { get; set; }
+
+        public DbSet<Reservation> Reservations { get; set; }
+    }
+}
---
+++ DolphinCove/Models/Reservation.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DolphinCove.Models
+{
+    public class Reservation
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime? Date { get; set; }
+
+        public DateTime? Schedule { get; set; }
+
+        public string? AdultParticipant { get; set; }
+
+        public string? ChildParticipant { get; set; }
+
+        public string? InfantParticipant { get; set; }
+
+        public decimal? UndiscountedTotal { get; set;}
+
+        public decimal? FinalTotal { get; set;}
+
+        public string? SpecialInstruction { get; set;}
+
+        public bool? BoolCruise { get; set;}
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? City { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+
+
+
+        //Relationship
+        public int? ExperienceId { get; set; }
+        [ForeignKey("ExperienceId")]
+        public virtual Experience? Experience { get; set; }
+
+
+
+        public int? PromotionCodeId { get; set; }
+        [ForeignKey("PromotionCodeId")]
+        public virtual PromotionCode? PromotionCode { get; set; }
+
+
+
+        public int? AddonId { get; set; }
+        [ForeignKey("AddonId")]
+        public virtual Addon? Addon { get; set; }
+
+
+
+        public int? CruiseId { get; set; }
+        [ForeignKey("CruiseId")]
+        public virtual Cruise? Cruise { get; set; }
+
+    }
+}
