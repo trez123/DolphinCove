94a18243f0da5e530826dd757ff9eb72d4b93952
Keando <Keando@LAPTOP-PLOR3GCE>
2023-08-22 11:04:36 -0500

    update_0

---
+++ DolphinCove/Controllers/AddonsController.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DolphinCove.Data;
+using DolphinCove.Models;
+using Microsoft.AspNetCore.Routing.Constraints;
+
+namespace DolphinCove.Controllers
+{
+    public class AddonsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public AddonsController(ApplicationDbContext context, IWebHostEnvironment webHostEnvironment)
+        {
+            _context = context;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        // GET: Addons
+        public async Task<IActionResult> Index()
+        {
+              return _context.Addons != null ? 
+                          View(await _context.Addons.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Addons'  is null.");
+        }
+
+        // GET: Addons/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Addons == null)
+            {
+                return NotFound();
+            }
+
+            var addon = await _context.Addons
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (addon == null)
+            {
+                return NotFound();
+            }
+
+            return View(addon);
+        }
+
+        // GET: Addons/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Addons/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,AddonName,AddonPrice,AddonImage1")] Addon addon)
+        {
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                string WebRoot = _webHostEnvironment.WebRootPath;
+                string uploadPath = WebRoot + AppConst.UploadPath;
+                string fileName = Guid.NewGuid().ToString();
+                string ext = Path.GetExtension(files[0].FileName);
+                using (var fs = new FileStream(Path.Combine(uploadPath, fileName + ext), FileMode.Create))
+                {
+                    files[0].CopyTo(fs);
+                }
+                addon.AddonImage1 =fileName + ext;
+                _context.Add(addon);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(addon);
+        }
+
+        // GET: Addons/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Addons == null)
+            {
+                return NotFound();
+            }
+
+            var addon = await _context.Addons.FindAsync(id);
+            if (addon == null)
+            {
+                return NotFound();
+            }
+            return View(addon);
+        }
+
+        // POST: Addons/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,AddonName,AddonPrice,AddonImage1")] Addon addon)
+        {
+            if (id != addon.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(addon);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AddonExists(addon.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(addon);
+        }
+
+        // GET: Addons/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Addons == null)
+            {
+                return NotFound();
+            }
+
+            var addon = await _context.Addons
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (addon == null)
+            {
+                return NotFound();
+            }
+
+            return View(addon);
+        }
+
+        // POST: Addons/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Addons == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Addons'  is null.");
+            }
+            var addon = await _context.Addons.FindAsync(id);
+            if (addon != null)
+            {
+                _context.Addons.Remove(addon);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AddonExists(int id)
+        {
+          return (_context.Addons?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ DolphinCove/Controllers/ExperiencesController.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DolphinCove.Data;
+using DolphinCove.Models;
+using Microsoft.AspNetCore.Hosting;
+
+namespace DolphinCove.Controllers
+{
+    public class ExperiencesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ExperiencesController(ApplicationDbContext context, IWebHostEnvironment webHostEnvironment)
+        {
+            _context = context;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        // GET: Experiences
+        public async Task<IActionResult> Index()
+        {
+              return _context.Experiences != null ? 
+                          View(await _context.Experiences.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Experiences'  is null.");
+        }
+
+        // GET: Experiences/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Experiences == null)
+            {
+                return NotFound();
+            }
+
+            var experience = await _context.Experiences
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (experience == null)
+            {
+                return NotFound();
+            }
+
+            return View(experience);
+        }
+
+        // GET: Experiences/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Experiences/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ExperienceName,ExperiencePrice,ExperienceImage1,ExperienceImage2,ExperienceImage3,ExperienceImage4")] Experience experience)
+        {
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                string WebRoot = _webHostEnvironment.WebRootPath;
+                string uploadPath = WebRoot + AppConst.UploadPathExp;
+                for (int i = 0; i < files.Count; i++)
+                {
+                    string filename = Guid.NewGuid().ToString();
+                    string ext = Path.GetExtension(files[i].FileName);
+                    using (var fs = new FileStream(Path.Combine(uploadPath, filename + ext), FileMode.Create))
+                    {
+                        files[i].CopyTo(fs);
+                    }
+                    if (i == 0)
+                  
+                        experience.ExperienceImage1 = filename + ext;
+                    if (i == 1)
+                        experience.ExperienceImage2 = filename + ext;
+                    if (i == 2)
+                        experience.ExperienceImage3 = filename + ext;
+                    if ( i == 3)
+                        experience.ExperienceImage4 = filename + ext;
+
+                }
+                _context.Add(experience);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(experience);
+        }
+
+        // GET: Experiences/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Experiences == null)
+            {
+                return NotFound();
+            }
+
+            var experience = await _context.Experiences.FindAsync(id);
+            if (experience == null)
+            {
+                return NotFound();
+            }
+            return View(experience);
+        }
+
+        // POST: Experiences/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ExperienceName,ExperiencePrice,ExperienceImage1,ExperienceImage2,ExperienceImage3,ExperienceImage4")] Experience experience)
+        {
+            if (id != experience.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(experience);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ExperienceExists(experience.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(experience);
+        }
+
+        // GET: Experiences/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Experiences == null)
+            {
+                return NotFound();
+            }
+
+            var experience = await _context.Experiences
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (experience == null)
+            {
+                return NotFound();
+            }
+
+            return View(experience);
+        }
+
+        // POST: Experiences/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Experiences == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Experiences'  is null.");
+            }
+            var experience = await _context.Experiences.FindAsync(id);
+            if (experience != null)
+            {
+                _context.Experiences.Remove(experience);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ExperienceExists(int id)
+        {
+          return (_context.Experiences?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
