4ab218ef0422d75a99afb7832030f926ee461bb7
Keando <Keando@LAPTOP-PLOR3GCE>
2023-08-28 09:10:34 -0500

    update_5

--- DolphinCove/Controllers/HomeController.cs
+++ DolphinCove/Controllers/HomeController.cs
@@ -1,16 +1,21 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using DolphinCove.Models;
+using Microsoft.EntityFrameworkCore;
+using DolphinCove.Data;
+using System.IO;
 
 namespace DolphinCove.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _dbContext;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext dbContext)
     {
         _logger = logger;
+        _dbContext = dbContext;
     }
 
     public IActionResult Index()
@@ -19,26 +24,102 @@ public class HomeController : Controller
     }
     public IActionResult SwimAdventure()
     {
-        return View();
+        int ExpId = 1;
+        string path = AppConst.UploadPathExp;
+        Experience? experience = _dbContext.Experiences.FirstOrDefault(e => e.Id == ExpId);
+        if (experience == null)
+        {
+            return NotFound();
+        }
+        string? experienceImage1 = path + experience.ExperienceImage1;
+        string? experienceImage2 = path + experience.ExperienceImage2;
+        string? experienceImage3 = path + experience.ExperienceImage3;
+        string? experienceImage4 = path + experience.ExperienceImage4;
+        ViewData["ExperienceImage1"] = experienceImage1;
+        ViewData["ExperienceImage2"] = experienceImage2;
+        ViewData["ExperienceImage3"] = experienceImage3;
+        ViewData["ExperienceImage4"] = experienceImage4;
+        return View(experience);
     }
     public IActionResult SharkEncounter()
     {
-        return View();
+        int ExpId = 2;
+        string path = AppConst.UploadPathExp;
+        Experience? experience = _dbContext.Experiences.FirstOrDefault(e => e.Id == ExpId);
+        if (experience == null)
+        {
+            return NotFound();
+        }
+        string? experienceImage1 = path + experience.ExperienceImage1;
+        string? experienceImage2 = path + experience.ExperienceImage2;
+        string? experienceImage3 = path + experience.ExperienceImage3;
+        string? experienceImage4 = path + experience.ExperienceImage4;
+        ViewData["ExperienceImage1"] = experienceImage1;
+        ViewData["ExperienceImage2"] = experienceImage2;
+        ViewData["ExperienceImage3"] = experienceImage3;
+        ViewData["ExperienceImage4"] = experienceImage4;
+        return View(experience);
     }
     public IActionResult Encounter()
     {
+        int ExpId = 3;
+        string path = AppConst.UploadPathExp;
+        Experience? experience = _dbContext.Experiences.FirstOrDefault(e => e.Id == ExpId);
+        if (experience == null)
+        {
+            return NotFound();
+        }
+        string? experienceImage1 = path + experience.ExperienceImage1;
+        string? experienceImage2 = path + experience.ExperienceImage2;
+        string? experienceImage3 = path + experience.ExperienceImage3;
+        string? experienceImage4 = path + experience.ExperienceImage4;
+        ViewData["ExperienceImage1"] = experienceImage1;
+        ViewData["ExperienceImage2"] = experienceImage2;
+        ViewData["ExperienceImage3"] = experienceImage3;
+        ViewData["ExperienceImage4"] = experienceImage4;
         return View();
     }
     public IActionResult AdmissionPlus()
     {
+        int ExpId = 4;
+        string path = AppConst.UploadPathExp;
+        Experience? experience = _dbContext.Experiences.FirstOrDefault(e => e.Id == ExpId);
+        if (experience == null)
+        {
+            return NotFound();
+        }
+        string? experienceImage1 = path + experience.ExperienceImage1;
+        string? experienceImage2 = path + experience.ExperienceImage2;
+        string? experienceImage3 = path + experience.ExperienceImage3;
+        string? experienceImage4 = path + experience.ExperienceImage4;
+        ViewData["ExperienceImage1"] = experienceImage1;
+        ViewData["ExperienceImage2"] = experienceImage2;
+        ViewData["ExperienceImage3"] = experienceImage3;
+        ViewData["ExperienceImage4"] = experienceImage4;
         return View();
     }
     public IActionResult DolphinRoyalSwim()
     {
+        int ExpId = 5;
+        string path = AppConst.UploadPathExp;
+        Experience? experience = _dbContext.Experiences.FirstOrDefault(e => e.Id == ExpId);
+        if (experience == null)
+        {
+            return NotFound();
+        }
+        string? experienceImage1 = path + experience.ExperienceImage1;
+        string? experienceImage2 = path + experience.ExperienceImage2;
+        string? experienceImage3 = path + experience.ExperienceImage3;
+        string? experienceImage4 = path + experience.ExperienceImage4;
+        ViewData["ExperienceImage1"] = experienceImage1;
+        ViewData["ExperienceImage2"] = experienceImage2;
+        ViewData["ExperienceImage3"] = experienceImage3;
+        ViewData["ExperienceImage4"] = experienceImage4;
         return View();
     }
     public IActionResult Privacy()
     {
+
         return View();
     }
 
